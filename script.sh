5cc1564fe0381d0473b6c8240e92fb9fdc229149
saulocurty <s240654@dac.unicamp.br>
2022-09-11 16:13:17 -0300

    Primeira etapa

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##Vamos assumir que o usuário so pode enviar uma flag 
+[ "$#" -eq 0 ] && { echo "Nenhum parametro passado"; exit 1; }
+
+f="$#"
+for element in "$@"; do
+  
+  if [ -d "$element" ]; then
+    rsync -av --delete $element/ /media/saulo/saulo
+  else 
+    echo "Error, $element não é um subdiretório do diretório atual"
+    exit 1
+  fi
+
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
