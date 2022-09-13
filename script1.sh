38e9c91da9f524c9b9b664b7c8788a094760594d
saulocurty <s240654@dac.unicamp.br>
2022-09-13 15:11:48 -0300

    rsyc corrigido

--- script1.sh
+++ script1.sh
@@ -2,20 +2,20 @@
 
 ##Vamos assumir que o usuário so pode enviar uma flag 
 [ "$#" -eq 0 ] && { echo "Nenhum parametro passado"; exit 1; }
-
+dest="/media/saulo/saulo"
 f="$#"
 string=""
 for element in "$@"; do
   
   if [ -d "$element" ]; then
-    $string+=" ${element}"
+    string+=" $element/"
   else 
     echo "Error, $element não é um subdiretório do diretório atual"
     exit 1
   fi
 
 done
-
+rsync -av $string $dest --delete
 echo "string é = $string"
 
 
